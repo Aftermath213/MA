06543af3fab5c75c5d73db219cf878d11503c909
DanSilvis <Daniel.Silvis@cga.ct.gov>
2017-08-07 12:29:52 -0400

    Include basic detail of how analysis could work.

---
+++ eda.R
@@ -0,0 +1,21 @@
+getwd()
+#generate connection to DB
+source("./connection.R")
+
+#load packages for data analysis
+library(dplyr)
+library(ggplot2)
+dbDisconnect(conEPM)
+#execute query to bring in one day of data
+
+
+
+#execute query to bring in PTP edits
+
+
+
+#plot claims distribution
+
+
+
+#run match on PTP edits and raw data
